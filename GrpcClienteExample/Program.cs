674a76665d50aa41243defb9704766fdd18d051c
Alfredo Quintana Lopez <alfreql@gmail.com>
2021-12-05 16:24:33 -0300

    otros

---
+++ GrpcClienteExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServiceExample;
+
+namespace GrpcClienteExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var request = new HelloRequest() {DeliveryDate = DateTime.UtcNow.ToTimestamp(), Name = "SomeName"};
+            var reply = await client.SayHelloAsync(request);
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
